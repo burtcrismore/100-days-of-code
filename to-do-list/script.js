6317692b8180142bd4e78d43d800fd772a320c94
Burt Crismore <burt@burtcrismore.com>
2021-05-02 19:41:29 -0700

    updated css and js

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,24 +1,81 @@
 // Create a to-do list
 
-// Select submit button and add task to list
 function addTask() {
-	const getToDo = document.querySelector('.todo');
-	const subBtn = getToDo.querySelector('.todo__submit-btn');
+	let getToDo = document.querySelector('.todo');
+	let subBtn = getToDo.querySelector('.todo__submit-btn');
+	let getField = getToDo.querySelector('.todo__submit-field');
+	let getDeletes = getToDo.querySelectorAll('.fa-times-circle');
+	let li = getToDo.querySelectorAll('.todo__list-item');
 
-	// function addElement() {
-	// 	console.log('function works');
+	// Add message if no tasks for the day
+	// if (true) {
+	// 	let message = getToDo.querySelector('.todo__message');
+	// 	let p = document.createElement('p');
+	// 	let textMessage = document.createTextNode(
+	// 		'Take a break. You have no tasks today.'
+	// 	);
 
+	// 	p.appendChild(textMessage);
+	// 	message.appendChild(p);
 	// }
 
-	subBtn.addEventListener('click', function (e) {
-		// console.log(e.currentTarget);
-		const getTaskVal = getToDo.querySelector('.todo__submit-field').value;
-		const newLi = document.createElement('li');
-		const newTask = document.createTextNode(getTaskVal);
-		newLi.appendChild(newTask);
-		const ul = document.querySelector('.todo__display-list');
-		ul.appendChild(newLi);
+	// If you have no tasks add a daily quote
+
+	// Check if return key is pressed
+	getField.addEventListener('keydown', function (e) {
+		if (e.code === 'Enter') {
+			subBtn.click();
+		}
 	});
+
+	// Display total count of tasks and tasks completed
+
+	// NOTE this for loop doesn't find the remove icon because the function runs before the li is created
+	// Remove task when icon clicked
+	function removeTask() {
+		console.log('it works');
+		// Search for all remove task buttons
+		for (const getDelete of getDeletes) {
+			getDelete.addEventListener('click', function (e) {
+				e.target.remove();
+				console.log('remove');
+			});
+		}
+	}
+	removeTask();
+
+	// Check if add button has been clicked
+	subBtn.addEventListener('click', function (e) {
+		// Check if task field is empty
+
+		let ul = getToDo.querySelector('.todo__list');
+
+		let newLi = document.createElement('li');
+		let text = document.createElement('span');
+		let circle = document.createElement('span');
+		let times = document.createElement('span');
+
+		function addClassName(varName, className1, className2) {
+			varName.classList.add(className1, className2);
+		}
+		addClassName(newLi, 'todo__list-item');
+		addClassName(text, 'todo__list-text');
+		addClassName(circle, 'fal', 'fa-circle');
+		addClassName(times, 'fal', 'fa-times-circle');
+		newLi.classList.remove('undefined');
+
+		let textVal = document.createTextNode(getField.value);
+
+		// newLi.appendChild(textVal);
+		ul.appendChild(newLi);
+		newLi.appendChild(text);
+		text.appendChild(textVal);
+		newLi.appendChild(circle);
+		newLi.appendChild(times);
+		getField.value = '';
+	});
+
+	// Mark task as complete when icon clicked
 }
 
 addTask();
