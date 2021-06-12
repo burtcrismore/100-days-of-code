fd98d2687d856978981b463a533b9838f5202d1d
Burt Crismore <burt@burtcrismore.com>
2021-06-12 00:53:13 -0700

    updates to css and js

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -1,17 +1,5 @@
 /* 
 =================================================
-Global variables
-================================================= 
-*/
-const playerChoice = document.querySelector('.player-choice__container');
-/* 
-=================================================
-Event listeners
-================================================= 
-*/
-
-/* 
-=================================================
 Game Options
 ================================================= 
 */
@@ -31,11 +19,12 @@ const gameOptions = {
 		computer: 100
 	},
 	scoring() {
-		if (this.score.human >= 5) {
-			console.log('Human wins!');
-		} else if (this.score.computer >= 5) {
+		if (this.meterBar.human === 0) {
 			console.log('Computer wins!');
+		} else if (this.meterBar.computer === 0) {
+			console.log('Human wins!');
 		}
+		return this;
 	}
 };
 
@@ -45,29 +34,30 @@ Start Round
 ================================================= 
 */
 const roundSteps = {
+	playerChoice: document.querySelector('.player-choice__container'),
 	allChoiceBtns: document.querySelectorAll('.player-choice__btn'),
 	faStacks: document.querySelectorAll('.fa-stack'),
 	meterBarHuman: document.querySelector('.scoreboard__meter-bar--human'),
 	meterBarComputer: document.querySelector('.scoreboard__meter-bar--computer'),
-	// panelHuman: document.querySelector('.results__panel--human'),
-	// panelComputer: document.querySelector('.results__panel--computer'),
 	humanRock: document.querySelector('.results__panel--human-rock'),
 	humanPaper: document.querySelector('.results__panel--human-paper'),
 	humanScissors: document.querySelector('.results__panel--human-scissors'),
 	computerRock: document.querySelector('.results__panel--computer-rock'),
 	computerPaper: document.querySelector('.results__panel--computer-paper'),
 	computerScissors: document.querySelector('.results__panel--computer-scissors'),
-
-	// handMove: document.querySelector('.results__panel--move-hand'),
+	messageWinner: document.querySelector('.results__divider-winner'),
+	messageLoser: document.querySelector('.results__divider-loser'),
+	messageDraw: document.querySelector('.results__divider-draw'),
 	disableBtn() {
-		console.log('disable button');
 		for (allChoiceBtn of this.allChoiceBtns) {
 			if (allChoiceBtn.disabled === false) {
+				// console.log('disable button');
 				allChoiceBtn.disabled = true;
 				allChoiceBtn.style.pointerEvents = 'none';
 			} else if (allChoiceBtn.disabled === true) {
-				allChoiceBtn.disabled = false;
-				allChoiceBtn.style.pointerEvents = 'auto';
+				// console.log('revert disable');
+				allChoiceBtn.removeAttribute('style');
+				allChoiceBtn.removeAttribute('disabled');
 			}
 		}
 		return this;
@@ -83,98 +73,110 @@ const roundSteps = {
 			this.humanRock.classList.add('results__panel--move-hand');
 			this.computerRock.classList.add('results__panel--move-hand');
 		}
-		// for (faStack of this.faStacks) {
-		// 	if (faStack.classList.contains('results__panel--move-hand')) {
-		// 		faStack.classList.remove('results__panel--move-hand');
-		// 	} else if (!faStack.classList.contains('results__panel--move-hand')) {
-		// 		faStack.classList.add('results__panel--move-hand');
-		// 	}
-		// }
 		return this;
 	},
 	handAnimationEnd(humanChoice, computerChoice) {
-		// currently only listens for the first handMove. need to add second listener or even delegation
-		const handMove = document.querySelector('.results__panel--move-hand');
-		handMove.addEventListener('animationend', () => {
-			console.log('animation ended');
+		// Added globalThis (or window.) to create global variables since these variables can't be called at runtime since they don't exist yet
+		globalThis.handMove = document.querySelector('.results__panel--move-hand');
+
+		globalThis.listenForHandMove = () => {
+			// console.log('animation ended');
 			this.changeHand(humanChoice, computerChoice);
 			this.whoWon(humanChoice, computerChoice);
-			this.resetRound(humanChoice, computerChoice);
-		});
-		// return this;
+
+			// Declare winner check
+			// Did you win or lose match
+
+			// Yes, end game and display replay button
+
+			// No, continue playing
+
+			globalThis.messageShow = document.querySelector('.results__divider-message--show');
+			messageShow.addEventListener('animationend', listenForMessageShow);
+		};
+
+		globalThis.listenForMessageShow = () => {
+			// console.log('message animation end');
+			setTimeout(() => {
+				// console.log('message removed');
+				messageShow.classList.remove('results__divider-message--show');
+
+				this.resetRound(humanChoice, computerChoice);
+			}, 1000);
+		};
+
+		handMove.addEventListener('animationend', listenForHandMove);
+
+		return this;
 	},
 	changeHand(humanChoice, computerChoice) {
-		// console.log(humanChoice);
+		console.log('player hand shown:', humanChoice);
+		console.log('computer hand shown:', computerChoice);
 		// console.log('check human choice');
 		// console.log(this.humanRock);
 
-		// if (humanChoice === 'Rock') {
-		// } else if (humanChoice === 'Paper') {
-		// 	this.humanRock.style.display = 'none';
-		// 	this.humanPaper.style.display = 'block';
-		// } else if (humanChoice === 'Scissors') {
-		// 	this.humanRock.style.display = 'none';
-		// 	this.humanScissors.style.display = 'block';
-		// }
-
-		// if (computerChoice === 'Rock') {
-		// } else if (computerChoice === 'Paper') {
-		// 	this.computerRock.style.display = 'none';
-		// 	this.computerPaper.style.display = 'block';
-		// } else if (computerChoice === 'Scissors') {
-		// 	this.computerRock.style.display = 'none';
-		// 	this.computerScissors.style.display = 'block';
-		// }
-		if (humanChoice != 'Rock') {
-			this.humanRock.style.display = 'none';
-
-			if (humanChoice === 'Paper') {
-				console.log('human paper icon');
-				this.humanPaper.style.display = 'block';
-			} else {
-				console.log('human scissors icon');
-				this.humanScissors.style.display = 'block';
-			}
+		// Add classes to add and remove instead of style.display
+		if (humanChoice === 'Paper') {
+			this.humanRock.classList.add('results__panel--hide');
+			this.humanPaper.classList.add('results__panel--show');
+			// this.humanRock.style.display = 'none';
+			// this.humanPaper.style.display = 'block';
+		} else if (humanChoice === 'Scissors') {
+			this.humanRock.classList.add('results__panel--hide');
+			this.humanScissors.classList.add('results__panel--show');
+			// this.humanRock.style.display = 'none';
+			// this.humanScissors.style.display = 'block';
 		}
 
-		if (computerChoice != 'Rock') {
-			this.computerRock.style.display = 'none';
-
-			if (computerChoice === 'Paper') {
-				console.log('computer paper icon');
-				this.computerPaper.style.display = 'block';
-			} else {
-				console.log('computer scissors icon');
-				this.computerScissors.style.display = 'block';
-			}
+		if (computerChoice === 'Paper') {
+			this.computerRock.classList.add('results__panel--hide');
+			this.computerPaper.classList.add('results__panel--show');
+		} else if (computerChoice === 'Scissors') {
+			this.computerRock.classList.add('results__panel--hide');
+			this.computerScissors.classList.add('results__panel--show');
 		}
+		// if (humanChoice != 'Rock') {
+		// 	this.humanRock.style.display = 'none';
 
-		// switch (humanChoice) {
-		// 	case 'Rock':
-		// 		console.log('Rock');
-		// 		break;
-		// 	case 'Paper':
-		// 		console.log('Paper');
-		// 		break;
-		// 	case 'Scissors':
-		// 		console.log('Scissors');
-		// 		break;
-		// 	default:
-		// 		console.log('Sorry, no hand.');
+		// 	if (humanChoice === 'Paper') {
+		// 		console.log('human paper icon');
+		// 		this.humanPaper.style.display = 'block';
+		// 	} else {
+		// 		console.log('human scissors icon');
+		// 		this.humanScissors.style.display = 'block';
+		// 	}
 		// }
+
+		// if (computerChoice != 'Rock') {
+		// 	this.computerRock.style.display = 'none';
+
+		// 	if (computerChoice === 'Paper') {
+		// 		console.log('computer paper icon');
+		// 		this.computerPaper.style.display = 'block';
+		// 	} else {
+		// 		console.log('computer scissors icon');
+		// 		this.computerScissors.style.display = 'block';
+		// 	}
+		// }
+
 		return this;
 	},
 	whoWon(humanChoice, computerChoice) {
 		if (humanChoice === computerChoice) {
-			console.log('you tied');
+			this.messageDraw.classList.add('results__divider-message--show');
+			console.log('message: draw');
 		}
 
 		// Did human pick rock?
 		else if (humanChoice === 'Rock') {
 			if (computerChoice === 'Scissors') {
+				console.log('message: winner');
+				this.messageWinner.classList.add('results__divider-message--show');
 				++gameOptions.score.human;
 				this.meterBarComputer.style.width = `${(gameOptions.meterBar.computer -= 20)}%`;
 			} else {
+				console.log('message: loser');
+				this.messageLoser.classList.add('results__divider-message--show');
 				++gameOptions.score.computer;
 				this.meterBarHuman.style.width = `${(gameOptions.meterBar.human -= 20)}%`;
 			}
@@ -182,9 +184,13 @@ const roundSteps = {
 		// Did human pick paper?
 		else if (humanChoice === 'Paper') {
 			if (computerChoice === 'Rock') {
+				console.log('message: winner');
+				this.messageWinner.classList.add('results__divider-message--show');
 				++gameOptions.score.human;
 				this.meterBarComputer.style.width = `${(gameOptions.meterBar.computer -= 20)}%`;
 			} else {
+				console.log('message: loser');
+				this.messageLoser.classList.add('results__divider-message--show');
 				++gameOptions.score.computer;
 				this.meterBarHuman.style.width = `${(gameOptions.meterBar.human -= 20)}%`;
 			}
@@ -192,40 +198,53 @@ const roundSteps = {
 		// Did human pick scissor?
 		else if (humanChoice === 'Scissors') {
 			if (computerChoice === 'Paper') {
+				console.log('message: winner');
+				this.messageWinner.classList.add('results__divider-message--show');
 				++gameOptions.score.human;
 				this.meterBarComputer.style.width = `${(gameOptions.meterBar.computer -= 20)}%`;
 			} else {
+				console.log('message: loser');
+				this.messageLoser.classList.add('results__divider-message--show');
 				++gameOptions.score.computer;
 				this.meterBarHuman.style.width = `${(gameOptions.meterBar.human -= 20)}%`;
 			}
 		}
+
+		console.log(gameOptions.meterBar.computer);
+		console.log(gameOptions.meterBar.human);
 	},
 	resetRound(humanChoice, computerChoice) {
-		// Buttons
-		this.disableBtn();
 		// Hands
-		if (humanChoice != 'Rock') {
-			this.humanRock.style.display = 'block';
-			if (humanChoice === 'Paper') {
-				this.humanPaper.style.display = 'none';
-			} else {
-				this.humanScissors.style.display = 'none';
-			}
+
+		// refactor with loop maybe?
+		if (humanChoice === 'Paper') {
+			this.humanRock.classList.remove('results__panel--hide');
+			this.humanPaper.classList.remove('results__panel--show');
+		} else if (humanChoice === 'Scissors') {
+			this.humanRock.classList.remove('results__panel--hide');
+			this.humanScissors.classList.remove('results__panel--show');
 		}
 
-		if (computerChoice != 'Rock') {
-			this.computerRock.style.display = 'block';
-			if (computerChoice === 'Paper') {
-				this.computerPaper.style.display = 'none';
-			} else {
-				this.computerScissors.style.display = 'none';
-			}
+		if (computerChoice === 'Paper') {
+			this.computerRock.classList.remove('results__panel--hide');
+			this.computerPaper.classList.remove('results__panel--show');
+		} else if (computerChoice === 'Scissors') {
+			this.computerRock.classList.remove('results__panel--hide');
+			this.computerScissors.classList.remove('results__panel--show');
 		}
 		// Animations
 		this.humanRock.classList.remove('results__panel--move-hand');
 		this.computerRock.classList.remove('results__panel--move-hand');
 
-		return this;
+		// Buttons
+		this.disableBtn();
+
+		// clear timeouts?
+		// clearTimeout();
+		// remove even listeners?
+		handMove.removeEventListener('animationend', listenForHandMove);
+		messageShow.removeEventListener('animationend', listenForMessageShow);
+		// return this;
 	}
 };
 
@@ -233,6 +252,9 @@ const startRound = (event) => {
 	const choiceBtn = event.target.closest('.player-choice__btn');
 	const computerChoice = gameOptions.choices[gameOptions.ranNum()];
 	const humanChoice = choiceBtn.dataset.choice;
+
+	console.log('human logged:', humanChoice);
+	console.log('computer logged:', computerChoice);
 
 	if (choiceBtn) {
 		roundSteps.disableBtn().handAnimation().handAnimationEnd(humanChoice, computerChoice);
@@ -249,10 +271,10 @@ const startRound = (event) => {
 		// Fade out announcement
 		// Rest button choices
 		//
-		// gameOptions.scoring();
+		gameOptions.scoring();
 	}
 };
-playerChoice.addEventListener('click', startRound);
+roundSteps.playerChoice.addEventListener('click', startRound);
 /* 
  
 =================================================
